10030ffe0143f25793f99041c809a8ddc8403d1c
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-11-18 13:39:30 +0000

    initial project structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,php}", 
+    "./public/**/*.{js,php}",
+    "./resources/**/*.{js,php}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
