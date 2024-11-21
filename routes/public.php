8cfb1014394ac73e6b776c1213cdc7f1dc363db0
iolite312 <68647023+iolite312@users.noreply.github.com>
2024-11-21 21:08:08 +0000

    routes

--- routes/public.php
+++ routes/public.php
@@ -1,18 +1,7 @@
 <?php
 
-namespace Routes;
-
 use App\Application\Router;
-$router = new Router;
 
-$router->get('/', function () {
-    return 'Welcome to the homepage!';
-});
+$router = Router::getInstance();
 
-$router->get('/about', function () {
-    return 'This is the about page.';
-});
-
-$router->post('/contact', function () {
-    return 'Form submitted!';
-});
+$router->get('/', [App\Controllers\HomeController::class, 'index']);
